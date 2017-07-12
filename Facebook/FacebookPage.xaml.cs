ed24048728a356533125131787fded157050d131
Alexa Helena Lòpez Serrano <Alexa@MacBook-Air-de-Alexa.local>
2017-07-11 23:40:29 -0500

    Interface FB

---
+++ Facebook/FacebookPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Facebook
+{
+    public partial class FacebookPage : ContentPage
+    {
+        public FacebookPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Facebook/Views/FbView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Facebook.Views
+{
+    public partial class FbView : ContentPage
+    {
+        public FbView()
+        {
+            InitializeComponent();
+        }
+    }
+}
